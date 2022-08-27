1e512b737fbce00f2d15e016e1f30fc0a663819e
Madhav Arora <madhavarora748@gmail.com>
2022-08-27 17:23:45 +0530

    Level 5: Adding Cookies and Sessions

--- app.js
+++ app.js
@@ -4,36 +4,45 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 // const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const session = require("express-session");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const url = process.env.DB_URL;
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
+    name: String,
     email: {
         type: String,
         required: true,
         unique: true
     },
-    password: {
-        type: String,
-        required: true
-    }
+    password: String
 });
 
-// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+userSchema.plugin(passportLocalMongoose, {usernameField: "email"});
 
 const User = mongoose.model("User", userSchema);
 
-const saltRounds = 12;
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 mongoose.connect(url).catch((err)=>{
     console.log(err);
@@ -43,53 +52,46 @@ app.get("/", (req, res)=>{
     res.render("home");
 });
 
-app.post("/signup", (req, res)=>{
-    const name = req.body.name;
-    const email = req.body.email;
+app.get("/account", (req, res)=>{
+    if (req.isAuthenticated()){
+        res.send("Perfecto!");
+    }
+    else {
+        res.redirect("/");
+    }
+});
 
-    bcrypt.hash(req.body.password, saltRounds, (err, password)=>{
+app.post("/signup", (req, res)=>{
+    User.register(new User({name: req.body.name, email: req.body.email}), req.body.password, (err, user)=>{
         if (err){
             console.log(err);
+            res.redirect("/");
         }
         else {
-            const user = new User({
-                name: name,
-                email: email,
-                password: password
-            });
-        
-            user.save((err)=>{
-                if (err){
-                    console.log(err);
-                }
-                else {
-                    res.send("Perfecto!");
-                }
+            passport.authenticate("local")(req, res, ()=>{
+                res.redirect("/account");
             });
         }
     });
 });
 
 app.post("/signin", (req, res)=>{
-    const email = req.body.email;
-
-    User.findOne({email: email}, (err, user)=>{
+    req.login(new User({email: req.body.email, password: req.body.password}), (err)=>{
         if (err){
             console.log(err);
+            res.redirect("/");
         }
         else {
-            if (user){
-                bcrypt.compare(req.body.password, user.password, (err, result)=>{
-                    if (result){
-                        res.send("Perfecto!");
-                    }
-                    else {
-                        res.redirect("/");
-                    }
-                });
-            }
+            passport.authenticate("local")(req, res, ()=>{
+                res.redirect("/account");
+            });
         }
     });
+});
+
+app.post("/signout", (req, res)=>{ // Post Route for Sign Out Button
+    req.logout();
+    res.redirect("/");
 });
 
 app.listen(3000, ()=>{
